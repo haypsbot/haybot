5f59b8172e0fab63c0dcd880537e375b395d6eb2
haypsbot <ashot.avanesyan17@gmail.com>
2026-02-12 23:21:35 +0400

    Ultimate bot.py

--- bot.py
+++ bot.py
@@ -228,7 +228,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-    
-    @dp.message()
-async def debug_id(message: types.Message):
-    print("CHAT ID:", message.chat.id)
